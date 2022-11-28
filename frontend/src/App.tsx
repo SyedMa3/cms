fae51d030be04be03a0b92e998cb024d2dd80a8a
SyedMa3 <sammateen10@outlook.com>
2022-11-28 13:25:55 +0530

    upcoming activites

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,12 +4,15 @@ import Carous from './components/Carous';
 import Navbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Upcoming from './components/Upcoming';
+import logo from './logo.svg';
+
 function App() {
   return (
     <div className="App">
       <Navbar bg='dark' variant='dark'>
         <Container>
           <Navbar.Brand>
+            <img src={logo} alt='logo'/>
             Test
           </Navbar.Brand>
         </Container>
--- frontend/src/components/Upcoming.tsx
+++ frontend/src/components/Upcoming.tsx
@@ -14,17 +14,17 @@ function Upcoming() {
                         <Col sm={4}>
                             <ListGroup>
                                 <ListGroup.Item action href="#link1">
-                                    Link 1
+                                    Date | Event 1
                                 </ListGroup.Item>
                                 <ListGroup.Item action href="#link2">
-                                    Link 2
+                                    Date | Event 2
                                 </ListGroup.Item>
                             </ListGroup>
                         </Col>
                         <Col sm={8}>
                             <Tab.Content>
                                 <Tab.Pane eventKey="#link1">
-                                    sss
+                                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                                 </Tab.Pane>
                                 <Tab.Pane eventKey="#link2">
                                     fff
