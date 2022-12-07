7f51bed01b01f4e460414f7a14a016f43d398234
SyedMa3 <sammateen10@outlook.com>
2022-12-07 23:02:21 +0530

    added removeEvent

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { addEvent, getEvents } from "../controllers/events/index"
+import { addEvent, getEvents, removeEvent } from "../controllers/events/index"
 
 const router: Router = Router()
 
 router.get("/events", getEvents)
 
 router.post("/events", addEvent)
+
+router.delete("/events/:id", removeEvent)
 
 export default router
