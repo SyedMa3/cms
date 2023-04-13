430fdfa37fb120b599ca05cac67ad150370c85be
SyedMa3 <sammateen10@outlook.com>
2023-04-13 23:27:47 +0530

    prettier -> formatter

--- backend/.eslintrc.js
+++ backend/.eslintrc.js
@@ -1,22 +1,22 @@
 module.exports = {
-  env: {
-    es2021: true,
-    node: true
-  },
-  extends: 'standard-with-typescript',
-  overrides: [
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module',
-    project: './tsconfig.json'
-  },
-  rules: {
-    "@typescript-eslint/no-misused-promises": [
-      "error",
-      {
-        "checksVoidReturn": false
-      }
-    ]
-  }
+    "env": {
+        "es2021": true,
+        "node": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:@typescript-eslint/recommended"
+    ],
+    "overrides": [
+    ],
+    "parser": "@typescript-eslint/parser",
+    "parserOptions": {
+        "ecmaVersion": "latest",
+        "sourceType": "module"
+    },
+    "plugins": [
+        "@typescript-eslint"
+    ],
+    "rules": {
+    }
 }
